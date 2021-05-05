778764367e295acb4d304c89acd2aaa3a00c9912
qqlexa <lewaglavaskii@gmail.com>
2021-05-05 16:00:09 +0300

    Change http://localhost:3000/ to /index. Runned RuboCop

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+# frozen_string_literal: true
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  root 'articles#index'
+
+  get '/articles', to: 'articles#index'
 end
