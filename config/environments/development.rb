c28a89f434b7174c5e394d70a739f03ab4e18bb2
qqlexa <lewaglavaskii@gmail.com>
2021-05-06 14:28:01 +0300

    Added Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+	
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -73,4 +74,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # 3) From https://guides.railsgirls.com/devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'articles#index'
+  devise_for :users
+  root to: 'articles#index'
 
-  get '/articles', to: 'articles#index'
+  resources :articles
+
+  # get '/articles', to: 'articles#index'
+  # get "/articles/:id", to: "articles#show"
 end
