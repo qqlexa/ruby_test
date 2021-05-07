95232d721e5b35180008e2dbb99a9376ca6c31bf
qqlexa <lewaglavaskii@gmail.com>
2021-05-07 14:19:41 +0300

    Migrated current_sing_in_at and last_sign_in_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_110859) do
+ActiveRecord::Schema.define(version: 2021_05_07_111404) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_05_07_110859) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
