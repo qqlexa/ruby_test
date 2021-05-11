3e28748cb0165966b10429b92c8d52ed22f25c18
qqlexa <lewaglavaskii@gmail.com>
2021-05-11 10:58:50 +0300

    Added model Item and Migrated

---
+++ db/migrate/20210511075537_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :title, null: false
+      t.text :body
+      t.integer :price, default: 0
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_112602) do
+ActiveRecord::Schema.define(version: 2021_05_11_075537) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body"
+    t.integer "price", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
